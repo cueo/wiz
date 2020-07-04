cac87036992f923ecbe9cef74d7c16badc954b72
cueo <mohit.ritanil@gmail.com>
2020-07-04 18:20:29 +0530

    Classes transferred over wire

---
+++ message.py
@@ -0,0 +1,35 @@
+import json
+from constants import GET_PILOT
+
+
+class Params:
+    def __init__(self, **kwargs):
+        self.params = kwargs
+
+    def __setitem__(self, key, value):
+        self.params[key] = value
+
+    def __str__(self):
+        return json.dumps(self.__dict__)
+
+
+class Request:
+    def __init__(self, method: str, params: Params = None):
+        self.method = method
+        if params:
+            self.params = params.params
+
+    def __bytes__(self):
+        return str(self).encode('utf-8')
+
+    def __str__(self):
+        return json.dumps(self.__dict__)
+
+
+class Response:
+    pass
+
+
+if __name__ == '__main__':
+    message = Request(GET_PILOT, Params(state=True))
+    print(bytes(message).decode('utf-8'))
