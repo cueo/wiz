c5562a9f799f12a165af63ee360d19791fea89b8
cueo <mohit.ritanil@gmail.com>
2020-07-04 18:19:47 +0530

    Common file to keep constants

---
+++ constants.py
@@ -0,0 +1,15 @@
+# message types
+GET_PILOT = "getPilot"
+SET_PILOT = "setPilot"
+
+# network calls
+PORT = 38899
+
+# Light keys
+HEXCODE = 'hexcode'
+COLOUR = 'colour'
+RED = 'red'
+GREEN = 'green'
+BLUE = 'blue'
+
+IP = '192.168.31.170'
